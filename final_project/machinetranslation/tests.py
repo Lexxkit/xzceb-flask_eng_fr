74b0c4adce5fac0697377d1232a30549aad0d1d4
Lexxkit <lexxkit@gmail.com>
2021-10-08 08:49:25 +0000

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 from ibm_cloud_sdk_core import api_exception
 
 
@@ -7,19 +7,21 @@ class TestTranslator(unittest.TestCase):
 
     def test_english_to_french_success(self):
             
-        self.assertEqual(englishToFrench("Hello"), "Bonjour")
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
 
     def test_english_to_french_empty_input(self):
 
-        self.assertRaises(api_exception.ApiException, englishToFrench, "")
+        with self.assertRaises(ValueError):
+            self.assertEqual(english_to_french(None),None)
 
     def test_french_to_english_success(self):
             
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
 
     def test_french_to_english_empty_input(self):
 
-        self.assertRaises(api_exception.ApiException, frenchToEnglish, "")
+        with self.assertRaises(ValueError):
+            self.assertEqual(french_to_english(None),None)
 
 
 if __name__ == '__main__':
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,10 @@
+'''
+Translator App
+'''
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,18 +20,24 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(f'{url}')
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
+    '''
+    Translates english to french
+    '''
     translation = language_translator.translate(
-        text=englishText,
+        text=english_text,
         model_id='en-fr').get_result()
 
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
+    '''
+    Translates french to english
+    '''
     translation = language_translator.translate(
-        text=frenchText,
+        text=french_text,
         model_id='fr-en').get_result()
 
-    englishText = translation['translations'][0]['translation']
-    return englishText
+    english_text = translation['translations'][0]['translation']
+    return english_text
